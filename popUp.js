0ab35b238bbd876b2ff45c09ba236e826080aba6
Daniel Vidal <126490535+Thiago-bsts@users.noreply.github.com>
2024-06-12 11:20:37 -0300

    Trabaia seu corno

---
+++ popUp.js
@@ -0,0 +1,8 @@
+document.getElementById('openpopUp').addEventListener('click', function() {
+    document.getElementById('backgraund-popup').style.display = 'block';
+    console.log('oi')
+  });
+  
+  document.getElementById('closePopup').addEventListener('click', function() {
+    document.getElementById('backgraund-popup').style.display = 'none';
+  });
